823a99643eced25e7627e045134535bfa4e38469
hanjsnote <enlun@naver.com>
2025-07-07 22:44:23 +0900

    자바 세션 강의

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import chapter1.MyNewClass;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       int a = 1;
+
+
+    }
+}
---
+++ src/chapter1/MyNewClass.java
@@ -0,0 +1,7 @@
+package chapter1;
+
+public class MyNewClass {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
