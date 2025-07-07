9bd7efc783600e273f9ca82de109bc6775c30afb
hanjsnote <enlun@naver.com>
2025-07-07 22:44:23 +0900

    자바 세션 강의

---
+++ src/chapter1/variable/Main.java
@@ -0,0 +1,17 @@
+package chapter1.variable;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        //[자료형] [변수이름];
+        int a;
+        a = 1;
+        a = 2;
+        System.out.println("a = " + a);
+        
+        //논리형
+        boolean booleanBox = true;
+        System.out.println("booleanBox = " + booleanBox);
+    }
+}
